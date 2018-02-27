438ca62d2b7bdc1c51472bb3877f23234c430b78
Alberto Monteiro <alberto.monteiro@live.com>
2018-02-26 21:29:14 -0300

    Adicionado projeto C# que se comunica com o Socket aberto na aplicação Delphi

---
+++ src/C#/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var tcpClient = new TcpClient();
+
+            tcpClient.Connect("localhost", 9090);
+
+            if (tcpClient.Connected)
+            {
+                var networkStream = tcpClient.GetStream();
+                var bytes = Encoding.ASCII.GetBytes("{ \"nome\": \"Gesiel\", \"status\": \"O host fulero\"}");
+                networkStream.Write(bytes, 0, bytes.Length);
+
+                tcpClient.Close();
+            }
+        }
+    }
+}
